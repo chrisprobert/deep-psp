29335da149e45a81bc19024fab8a8fa7881f4865
Chris Probert <chris@CMBP.local>
2015-05-09 14:54:08 -0700

    add shell script to run uniprot conversion

---
+++ uniprot_parser/convert_uniprot_xml.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+source /home/${USER}/.bash_profile
+
+#
+# convert the given XML file to TSV
+#
+
+
+# source directory for uniprot parser
+export SOURCE_DIR=/home/${USER}/deep-psp/uniprot_parser/
+
+# uniprot data directory
+export UNIPROT_DIR=${PI_HOME}/uniprot_data/
+
+# uniprot file to convert
+export UNIPROT_XML=${UNIPROT_DIR}uniprot.xml
+
+# output TSV file
+export UNIPROT_TSV=${UNIPROT_XML}uniprot.xml.tsv
+
+
+python ${SOURCE_DIR}xml_to_tsv.py --uniprotXML ${UNIPROT_XML} --outputTSV ${UNIPROT_TSV}
+
