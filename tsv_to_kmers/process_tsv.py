a4804bfe66ab8699b74bae50798a4d2aae7e9e1f
= <=>
2015-05-12 18:43:06 -0700

    remove length restrictions

--- tsv_to_kmers/process_tsv.py
+++ tsv_to_kmers/process_tsv.py
@@ -19,14 +19,11 @@ def main() :
 	parser.add_argument('--inputTSV', help='input TSV file', required=True)
 	parser.add_argument('--outputTSV', help='output TSV file', required=True)
 	parser.add_argument('--annot', help='annotation to select', required=True)
-	parser.add_argument('--minlen', help='minimum length', required=True)
-	parser.add_argument('--maxlen', help='maximum length', required=True)
 	args = parser.parse_args()
 
-	filterTSV(annot=args.annot, maxlen=args.maxlen, minlen=args.minlen,
-		input_tsv=args.inputTSV, output_path=args.outputTSV)
+	filterTSV(annot=args.annot, input_tsv=args.inputTSV, output_path=args.outputTSV)
 
-def filterTSV(annot, maxlen, minlen, input_tsv, output_path) :
+def filterTSV(annot, input_tsv, output_path) :
 
 	"""
 	Get all of the specified annotation regions from a TSV that have
@@ -36,12 +33,10 @@ def filterTSV(annot, maxlen, minlen, input_tsv, output_path) :
 	with open(output_path, 'w') as out_f :
 		for line in open(input_tsv) :
 			l = line.strip().split('\t')
-			print l
 			if len(l) < 6 or l[1] != annot : continue
 			start = int(l[3])
 			stop = int(l[4])
 			length = stop - start
-			if length < minlen or length > maxlen : continue
 			seq = l[5][start-1:stop-1] # positions are 1-based
 			out_f.write('%s\t%s\t%s\t%s' % (l[0], l[1], l[2], seq))
 
