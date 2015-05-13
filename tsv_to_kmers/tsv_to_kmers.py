edc8f9b895b64b87152f06014a52ecc88f49ea63
Chris Probert <chris@DN0a27221e.SUNet>
2015-05-13 16:44:50 -0700

    implemented all-in-one tsv to kmers scrip

--- tsv_to_kmers/tsv_to_kmers.py
+++ tsv_to_kmers/tsv_to_kmers.py
@@ -3,6 +3,7 @@
 import argparse
 from collections import Counter
 from kmer_utils import getKmers, getKmerCounts
+import subprocess
 from operator import add
 
 """
@@ -19,38 +20,56 @@ def main() :
 
   parser = argparse.ArgumentParser(description='Filter a uniprot TSV based on annotation')
   parser.add_argument('--inputTSV', help='input TSV file, in format of process_tsv.py', required=True)
+  parser.add_argument('--kmerList', help='kmer list file', required=True)
   parser.add_argument('--outputTSV', help='output TSV kmer file', required=True)
   parser.add_argument('--numCts', help='Maximum number of counts to select', required=True, type=int)
   parser.add_argument('--k', help='length of k-mers', required=True, type=int)
   args = parser.parse_args()
 
-  seqsToCounts(k=int(args.k), numCts=int(args.numCts), input_tsv=args.inputTSV, output_path=args.outputTSV)
+  k = int(args.k)
+  numCts = int(args.numCts)
 
-def seqsToCounts(k, numCts, input_tsv, output_path) :
+  writeAllKmers(k, args.inputTSV, args.kmerList)
+  sortedKmerList = getSortedKmerList(args.kmerList, numCts)
+  seqsToCounts(k, sortedKmerList, args.inputTSV, args.outputTSV)
 
+def writeAllKmers(k, input_tsv, kmerList) :
+  """
+  Write all the kmers present in each sequence to a file
+  """
+
+  with open(kmerList, 'w') as out_f :
+    for line in open(input_tsv) :
+      l = line.strip().split('\t')
+      if len(l) < 4 : continue
+      for kmer in list(getKmerCounts(l[3], k).elements()) :
+        out_f.write(kmer + '\n')
+
+
+def getSortedKmerList(kmerList, numCts) :
+  """
+  Sort the kmer list by number of occurances. Return a list of the top numCts kmers in sorted order.
+  """
+
+  result = subprocess.check_output(["cat", kmerList, "|", "sort", "|", "uniq", "-c", "|", "sort", "-rn", "|", "awk", "{print $2}", "|", "head", "-n", str(numCts)])
+  return result.strip().split()
+
+
+def seqsToCounts(k, sortedKmerList, input_tsv, output_path) :
   """
   Reduce each sequence to a set of counts
   """
 
-  # first pass through to get the total set of counts
-  total_counts = []
-  for line in open(input_tsv) :
-    l = line.strip().split('\t')
-    if len(l) < 4 : continue
-    total_counts.append(getKmerCounts(l[3], k))
-
-  top_counts_list = list(reduce(add, total_counts).most_common(numCts))
-
   with open(output_path, 'w') as out_f :
-    out.write('#' + '\t'.join(top_counts_list) + '\n')
-
+    out_f.write('#' + '\t'.join(sortedKmerList) + '\n')
     for line in open(input_tsv) :
       l = line.strip().split('\t')
       if len(l) < 4 : continue
       counts = getKmerCounts(l[3], k)
-      num_kmers = sum(counts.values())
-      kmers = set(counts)
-      counts = map(lambda x: str(float(counts[x]) / num_kmers) if x in kmers else str(0), top_counts_list)
+      num_kmers = len(l[3]) - k + 1
+      kmers_set = set(counts)
+      counts = map(lambda x: str(float(counts[x]) / num_kmers) if x in kmers else str(0), sortedKmerList)
       out_f.write('\t'.join(counts) + '\n')
 
 if __name__ == '__main__' : main()
+
