f2cfea307f68d1675560030bcb1373ae3d0c7737
Chris Probert <chris@DN0a2333cc.SUNet>
2015-05-28 16:56:50 -0700

    add sequence feature extraction

---
+++ keras/extract_seqs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#
+# Simple bash script to extract sequences for given labels
+#
+
+UNIPROT_TSV=../../uniprot_sprot.xml.tsv
+OUTPUT_DIR=../../seq_features/
+
+
+# schema of uniprot TSV file:
+# id	seq_feature.type	seq_feature.description	start	stop	seq
+
+for feature in chain transmembrane-region strand helix
+do
+	grep $feature $UNIPROT_TSV | awk '{if(length($1) > 9) {print $1}}' > ${OUTPUT_DIR}${feature}_seqs.txt
+done
+
