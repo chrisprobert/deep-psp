c8f0d088a832525ea24147b72fb9641d016b8507
Chris Probert <chris@DN0a2333cc.SUNet>
2015-05-28 17:54:51 -0700

    fix global case

--- keras/get_random_seq_features.sh
+++ keras/get_random_seq_features.sh
@@ -15,8 +15,8 @@ GLOBAL_DISTR=${OUTPUT_DIR}uniprot_seqs.txt
 
 
 # get the background sequence distribution
-sed "1d" $UNIPROT_TSV | awk '{if(length($6) > 9) {print $6}}' | tr '[:lower:]' '[:upper:]' > $GLOBAL_DISTR
-fold -w 1 $GLOBAL_DISTR | sort | uniq -c > $seq_distr_annot &
+sed "1d" $UNIPROT_TSV | awk '{if(length($6) > 9) {print $6}}' | tr '[:lower:]' '[:upper:]' > ${OUTPUT_DIR}all_seqs.txt
+fold -w 1 ${OUTPUT_DIR}all_seqs.txt | sort | uniq -c > $GLOBAL_DISTR &
 
 
 # get background distr per-feature
