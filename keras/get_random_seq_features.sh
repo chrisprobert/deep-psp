f18331d1f1e569df8680c1ab6c27071a90bc08ab
Chris Probert <chris@DN0a2333cc.SUNet>
2015-05-28 18:27:48 -0700

    add inputSeqs arg

--- keras/get_random_seq_features.sh
+++ keras/get_random_seq_features.sh
@@ -14,31 +14,36 @@ GLOBAL_DISTR=${OUTPUT_DIR}uniprot_seqs.txt
 # id	seq_feature.type	seq_feature.description	start	stop	seq
 
 
-# get the background sequence distribution
-sed "1d" $UNIPROT_TSV | shuf -n 100000 | awk '{if(length($6) > 9) {print $6}}' | tr '[:lower:]' '[:upper:]' > ${OUTPUT_DIR}all_seqs.txt
-fold -w 1 ${OUTPUT_DIR}all_seqs.txt | sort | uniq -c > $GLOBAL_DISTR &
+# # get the background sequence distribution
+# sed "1d" $UNIPROT_TSV | shuf -n 100000 | awk '{if(length($6) > 9) {print $6}}' | tr '[:lower:]' '[:upper:]' > ${OUTPUT_DIR}all_seqs.txt
+# fold -w 1 ${OUTPUT_DIR}all_seqs.txt | sort | uniq -c > $GLOBAL_DISTR &
 
 
-# get background distr per-feature
-for feature in chain transmembrane-region strand helix
-do
-	# get per-feature distribution
-	fold -w 1 ${OUTPUT_DIR}${feature}_seqs.txt | sort | uniq -c > ${OUTPUT_DIR}${feature}_distr.txt &
+# # get background distr per-feature
+# for feature in chain transmembrane-region strand helix
+# do
+# 	# get per-feature distribution
+# 	fold -w 1 ${OUTPUT_DIR}${feature}_seqs.txt | sort | uniq -c > ${OUTPUT_DIR}${feature}_distr.txt &
 
-done
+# done
 
 
 
-# let all the distr counters finish
-wait
+# # let all the distr counters finish
+# wait
 
 
 
 for feature in chain transmembrane-region strand helix
 do
 	# do with background distribution
-	../tsv_to_kmers/generate_random_seqs.py --seqDistr $GLOBAL_DISTR --outputSeqs ${OUTPUT_DIR}${feature}_globalbackground_seqs.txt &
-	../tsv_to_kmers/generate_random_seqs.py --seqDistr ${OUTPUT_DIR}${feature}_distr.txt --outputSeqs ${OUTPUT_DIR}${feature}_featurebackground_seqs.txt &
+	../tsv_to_kmers/generate_random_seqs.py --seqDistr $GLOBAL_DISTR \
+		--outputSeqs ${OUTPUT_DIR}${feature}_globalbackground_seqs.txt \
+		--inputSeqs ${OUTPUT_DIR}${feature}_seqs.txt &
+	# do with feature background distribution
+	../tsv_to_kmers/generate_random_seqs.py --seqDistr ${OUTPUT_DIR}${feature}_distr.txt \
+		--outputSeqs ${OUTPUT_DIR}${feature}_featurebackground_seqs.txt \
+		--inputSeqs ${OUTPUT_DIR}${feature}_seqs.txt &
 
 done
 
